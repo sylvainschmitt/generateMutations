1c21622db1da3a1ec47eb37094a14d6ebcd599b5
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2022-03-30 13:40:43 -0300

    reduce experiment

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# wget https://urgi.versailles.inra.fr/download/oak/Qrob_PM1N.fa.gz
-# gunzip Qrob_PM1N.fa.gz
-# wget https://urgi.versailles.inra.fr/download/oak/Qrob_PM1N_refTEs.gff.gz
-# wget https://urgi.versailles.inra.fr/download/oak/Qrob_PM1N_genes_20161004.gff.gz
+wget https://urgi.versailles.inra.fr/download/oak/Qrob_PM1N.fa.gz
+gunzip Qrob_PM1N.fa.gz
 wget www.oakgenome.fr/wp-content/uploads/files/Heterozygous_sites_3P.zip
 unzip Heterozygous_sites_3P.zip
 unzip Heterozygous_sites_3P/3P.haplov2.3.bam.pisorted.dedup.pileup.mincov100-maxcov200.vcf.withoutindel.sed.siteshz.zip 
